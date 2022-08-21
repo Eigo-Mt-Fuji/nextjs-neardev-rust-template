b9e25e918a2f3deeeca7b3695c222c369c7f5c23
user.email <efg.river@gmail.com>
2022-08-22 07:22:49 +0900

    update steady-study-token/src/lib.rs with ft_report_study_commit

--- contracts/steady-study-token/src/lib.rs
+++ contracts/steady-study-token/src/lib.rs
@@ -1,9 +1,11 @@
 // use for NEP-141 Fungible Token
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{
-    env, log, near_bindgen, AccountId, Balance, BorshStorageKey, PanicOnDefault,
+    env, log, near_bindgen, require, AccountId, Balance, BorshStorageKey, PanicOnDefault,
     PromiseOrValue,
 };
+use serde_json::json;
+
 use near_sdk::collections::LazyOption;
 use near_sdk::json_types::U128;
 
@@ -12,11 +14,14 @@ use near_contract_standards::fungible_token::metadata::{
     FungibleTokenMetadata, FungibleTokenMetadataProvider, FT_METADATA_SPEC,
 };
 
+use near_sdk::collections::UnorderedMap;
+
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 struct SteadyStudyTokenContract {
     token: FungibleToken,
     metadata: LazyOption<FungibleTokenMetadata>,
+    used_urls: UnorderedMap<String, AccountId>,
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
@@ -25,6 +30,7 @@ const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://
 enum StorageKey {
     FungibleToken,
     Metadata,
+    UsedUrls,
 }
 
 #[near_bindgen]
@@ -45,7 +51,7 @@ impl SteadyStudyTokenContract {
                 reference: None,
                 reference_hash: None,
                 decimals: 24,
-            },
+            }
         )
     }
 
@@ -56,31 +62,61 @@ impl SteadyStudyTokenContract {
         assert_eq!(!env::state_exists(), true, "Already initialized");
         metadata.assert_valid();
         let mut this = Self {
+            // near_contract_standards::fungible_token::core_impl::FungibleToken's new 
             token: FungibleToken::new(StorageKey::FungibleToken),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
+            used_urls: UnorderedMap::new(StorageKey::UsedUrls),
         };
+        
         this.token.internal_register_account(&owner_id);
         this.token.internal_deposit(&owner_id, total_supply.into());
         this
     }
+    
+    ///
+    /// ft_report_study_commit 
+    /// 
+    pub fn ft_report_study_commit(&mut self, receiver_id: AccountId, memo: Option<String>, urls: &Vec<String>) {
+        // https://docs.rs/near-sdk/2.0.1/near_sdk/collections/struct.Vector.html#method.len
+        require!(urls.len() == 3, "Need 3 urls commit.");
 
+        // msg内のurlsがの消込済みかを判定する
+        // https://docs.rs/near-sdk/2.0.1/near_sdk/collections/struct.Vector.html#method.iter
+        for element in urls.iter() {
+            require!(self.used_urls.get(&element.to_string()) == None, "Not allows same url commit.");
+        }
+        let sender_account_id = env::signer_account_id();
+
+        // URL消込
+        for element in urls.iter() {
+            self.used_urls.insert(element, &sender_account_id);
+        }
+        // 
+        let owner_account_id = env::current_account_id();
+        println!("owner_account_id: {}", &owner_account_id);
+        if self.token.accounts.get(&receiver_id) == None {
+            self.token.internal_register_account(&receiver_id);
+        }
+        self.token.internal_transfer(
+            &owner_account_id,
+            &receiver_id,
+            1,
+            memo
+        );    
+
+    }
+
+    pub fn ft_data_to_msg(&self, urls: &Vec<String> ) -> String {
+        json!({"urls":urls}).to_string()
+    }
     fn on_account_closed(&mut self, account_id: AccountId, balance: Balance) {
         log!("Closed @{} with {}", account_id, balance);
     }
-
     fn on_tokens_burned(&mut self, account_id: AccountId, amount: Balance) {
         log!("Account @{} burned {}", account_id, amount);
     }
-
 }
 
-// `ident`識別子は関数・変数の名前用の識別子
-// https://doc.rust-jp.rs/rust-by-example-ja/macros/designators.html
-// near_contract_standards::impl_fungible_token_core! 
-// https://docs.rs/near-contract-standards/latest/src/near_contract_standards/fungible_token/macros.rs.html#4-59
-// this means that impl FungibleTokenCore trait and FungibleTokenResolver trait for SteadyStudyTokenContract
-near_contract_standards::impl_fungible_token_core!(SteadyStudyTokenContract, token, on_tokens_burned);
-near_contract_standards::impl_fungible_token_storage!(SteadyStudyTokenContract, token, on_account_closed);
 
 #[near_bindgen]
 impl FungibleTokenMetadataProvider for SteadyStudyTokenContract {
@@ -89,51 +125,72 @@ impl FungibleTokenMetadataProvider for SteadyStudyTokenContract {
     }
 }
 
-// use $crate::fungible_token::core::FungibleTokenCore;
-// impl FungibleTokenCore for SteadyStudyTokenContract {
-    // #[payable]
-    // fn ft_transfer(
-    //     &mut self,
-    //     receiver_id: AccountId,
-    //     amount: U128,
-    //     memo: Option<String>,
-    // ) {
-    //     self.$token.ft_transfer(receiver_id, amount, memo)
-    // }
+near_contract_standards::impl_fungible_token_core!(SteadyStudyTokenContract, token, on_tokens_burned);
+near_contract_standards::impl_fungible_token_storage!(SteadyStudyTokenContract, token, on_account_closed);
 
-    // #[payable]
-    // fn ft_transfer_call(
-    //     &mut self,
-    //     receiver_id: AccountId,
-    //     amount: U128,
-    //     memo: Option<String>,
-    //     msg: String,
-    // ) -> PromiseOrValue<U128> {
-    //     self.$token.ft_transfer_call(receiver_id, amount, memo, msg)
-    // }
+#[cfg(all(test, not(target_arch = "wasm32")))]
+mod tests {
+    use near_sdk::test_utils::{accounts, VMContextBuilder};
+    use near_sdk::{testing_env, Balance};
+    use super::*;
+    const TOTAL_SUPPLY: Balance = 1_000_000_000_000_000;
 
-    // fn ft_total_supply(&self) -> U128 {
-    //     self.$token.ft_total_supply()
-    // }
+    fn get_context(predecessor_account_id: AccountId) -> VMContextBuilder {
+        let mut builder = VMContextBuilder::new();
+        builder
+            .current_account_id(accounts(0))
+            .signer_account_id(predecessor_account_id.clone())
+            .predecessor_account_id(predecessor_account_id);
+        builder
+    }
 
-    // fn ft_balance_of(&self, account_id: AccountId) -> U128 {
-    //     self.$token.ft_balance_of(account_id)
-    // }
-// }
-// use $crate::fungible_token::resolver::FungibleTokenResolver;
-// impl FungibleTokenResolver for SteadyStudyTokenContract {
-    // #[private]
-    // fn ft_resolve_transfer(
-    //     &mut self,
-    //     sender_id: AccountId,
-    //     receiver_id: AccountId,
-    //     amount: U128,
-    // ) -> U128 {
-    //     let (used_amount, burned_amount) =
-    //         self.$token.internal_ft_resolve_transfer(&sender_id, receiver_id, amount);
-    //     if burned_amount > 0 {
-    //         $(self.$on_tokens_burned_fn(sender_id, burned_amount);)?
-    //     }
-    //     used_amount.into()
-    // }
-// }
+    #[test]
+    fn test_ft_data_to_msg() {
+        let context = get_context(accounts(1));
+        testing_env!(context.build());
+        let contract = SteadyStudyTokenContract::new_default_meta(accounts(1).into(), TOTAL_SUPPLY.into());
+
+        let data: Vec<String> = vec!["ipfs://hoge".to_string(), "ipfs://fuga".to_string(), "ipfs://poyo".to_string()];
+        let msg = contract.ft_data_to_msg(&data);
+        assert_eq!(msg, "{\"urls\":[\"ipfs://hoge\",\"ipfs://fuga\",\"ipfs://poyo\"]}");
+    }
+    #[test]
+    #[should_panic(expected = "The contract is not initialized")]
+    fn test_default() {
+        let context = get_context(accounts(1));
+        testing_env!(context.build());
+        let _contract = SteadyStudyTokenContract::default();
+    }
+    #[test]
+    fn test_ft_report_study_commit() {
+        let mut context = get_context(accounts(2));
+        testing_env!(context.build());
+        let mut contract = SteadyStudyTokenContract::new_default_meta(accounts(0).into(), TOTAL_SUPPLY.into());
+        testing_env!(context
+            .storage_usage(env::storage_usage())
+            .attached_deposit(contract.storage_balance_bounds().min.into())
+            .predecessor_account_id(accounts(1))
+            .build());
+        // Paying for account registration, aka storage deposit
+        contract.storage_deposit(None, None);
+
+        testing_env!(context
+            .storage_usage(env::storage_usage())
+            .attached_deposit(1)
+            .predecessor_account_id(accounts(2))
+            .build());
+
+        let data: Vec<String> = vec!["ipfs://hoge".to_string(), "ipfs://fuga".to_string(), "ipfs://poyo".to_string()];
+        contract.ft_report_study_commit( accounts(1), None, &data);
+
+        testing_env!(context
+            .storage_usage(env::storage_usage())
+            .account_balance(env::account_balance())
+            .is_view(true)
+            .attached_deposit(0)
+            .build());
+
+        assert_eq!(contract.ft_balance_of(accounts(0)).0, (TOTAL_SUPPLY - 1));
+        assert_eq!(contract.ft_balance_of(accounts(1)).0, 1);
+    }
+}   
